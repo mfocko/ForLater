5db1b55369e486890b5aa7c9f263731fd5e0cdcd
Matej Focko <matej.focko@outlook.com>
2020-07-18 11:58:43 +0200

    Initial commit

---
+++ ForLater/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ForLater {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            var p = new System.Diagnostics.Process();
+        }
+    }
+}
